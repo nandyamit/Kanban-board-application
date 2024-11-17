89dff937df4b0a553a4501a5aacd6bfe903eb7e4
nandyamit <nandyamit.in@gmail.com>
2024-11-16 20:51:42 -0800

    Prepare for deployment

--- client/src/utils/config.ts
+++ client/src/utils/config.ts
@@ -1,4 +1,2 @@
 // client/src/utils/config.ts
-export const API_BASE_URL = process.env.NODE_ENV === 'production' 
-  ? 'https://your-render-app-url.onrender.com'
-  : 'http://localhost:3001';
+export const API_BASE_URL = 'http://localhost:3001';
--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,26 +1,24 @@
-// server/src/models/index.ts
+import dotenv from 'dotenv';
+dotenv.config();
+
 import { Sequelize } from 'sequelize';
+import { UserFactory } from './user.js';
+import { TicketFactory } from './ticket.js';
 
-const env = process.env.NODE_ENV || 'development';
-let sequelize: Sequelize;
+const sequelize = process.env.DB_URL
+  ? new Sequelize(process.env.DB_URL)
+  : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
+      host: 'localhost',
+      dialect: 'postgres',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
 
-if (env === 'production') {
-  sequelize = new Sequelize(process.env.DATABASE_URL!, {
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    }
-  });
-} else {
-  sequelize = new Sequelize({
-    database: 'kanban_db',
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: 'localhost',
-    dialect: 'postgres'
-  });
-}
+const User = UserFactory(sequelize);
+const Ticket = TicketFactory(sequelize);
 
-export { sequelize };
+User.hasMany(Ticket, { foreignKey: 'assignedUserId' });
+Ticket.belongsTo(User, { foreignKey: 'assignedUserId', as: 'assignedUser'});
+
+export { sequelize, User, Ticket };
